bc558bf9f410dcb492bb8c6b7a94bd96943ee018
Richard <skynet.tw@gmail.com>
2022-03-15 23:05:49 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "甲乙丙丁戊己庚辛壬癸";
+            string b = "子丑寅卯辰巳午未申酉戌亥";
+            int x = 0;
+            int y = 0;
+            for (int i=0; i<60; i++)
+            {
+                Console.WriteLine(a[x].ToString() + b[y].ToString());
+                if (++x >= a.Length) x = 0;
+                if (++y >= b.Length) y = 0;
+            }
+            Console.ReadLine();
+        }
+    }
+}
